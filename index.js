e3896805056e3b77f148112739339394c97cb59e
Karina Pliego <karinapliegovergara@gmail.com>
2023-04-29 14:20:27 -0500

    completed functions lab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(someValue) {
+    return Math.abs(someValue - 42);
+}
+
+function distanceFromHqInFeet (someValue) {
+    return distanceFromHqInBlocks(someValue) * 264;
+}
+
+function distanceTravelledInFeet(start, destination) {
+    return Math.abs((destination - start) * 264);
+}
+
+function calculatesFarePrice(start, destination) {
+   if (distanceTravelledInFeet(start, destination) >= 400 && distanceTravelledInFeet(start, destination) <= 2000) {
+       return ((distanceTravelledInFeet(start, destination) - 400) * 2) / 100;
+   } else if (distanceTravelledInFeet(start, destination) > 2000 && distanceTravelledInFeet(start, destination) < 2500) {
+       return 25;
+   } else if (distanceTravelledInFeet(start, destination) > 2500) {
+       return 'cannot travel that far';
+   }
+   return 0;
+}
